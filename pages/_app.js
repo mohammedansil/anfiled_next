d555caad43dd128aba989248516104cce310ba65
mohammedansil <anzilmohd4@gmail.com>
2022-08-11 22:42:02 +0530

    git cache cleared

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import Layout from "../Components/layout";
 import GlobalStyle from "../components/globalStyles";
 function MyApp({ Component, pageProps }) {
   return (
